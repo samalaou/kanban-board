3272ad04bf4ca27826b75b6e1af56319240bbeb8
Sam <sam.mjalaoui@gmail.com>
2024-08-06 15:52:03 +0200

    add Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Footer from './components/Footer'
 import NavBar from './components/NavBar'
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
     <NavBar/>
+    <Footer/>
     </>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import './Footer.css'
+
+function Footer() {
+  return (
+    <footer className="Footer">
+      <a href="https://github.com/samalaou/kanban-board/" target="_blank">
+        View on GitHub
+      </a>
+    </footer>
+  );
+}
+
+export default Footer;
