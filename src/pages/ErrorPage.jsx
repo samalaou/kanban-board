05b7c7e7470e63176d757773ec2b780fa795cdbd
Sam <sam.mjalaoui@gmail.com>
2024-08-08 15:05:37 +0200

    Improve ErrorPage

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,5 +1,13 @@
+import { Link } from 'react-router-dom';
+
 function ErrorPage() {
-    return <div>Error Page</div>;
+    return (
+        <div className="ErrorPage">
+            <h1>404 - Page Not Found</h1>
+            <p>Oops! The page you&apos;re looking for doesn&apos;t exist or has been moved.</p>
+            <Link className="home-link" to="/">Go to Homepage</Link>
+        </div>
+    );
 }
-  
+
 export default ErrorPage;
